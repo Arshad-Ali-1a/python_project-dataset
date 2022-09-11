058bc750d5a02aff98ba34f08240be130c8811c1
Arshad-Ali-1a <21bd1a661acsma@gmail.com>
2022-09-11 14:45:09 +0530

    created django webpage

---
+++ web/django/eamcet_colleges/home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from pathlib import Path
+import sys
+# Create your views here.
+
+_pp = str(Path().resolve()).split("\\")
+_ind = len(_pp)-1-(_pp[::-1].index("python_project-dataset"))
+base_path = "\\".join(_pp[:_ind+1])
+print(base_path)
+sys.path.append(base_path)
+
+from colleges import *
+
+def index(request):
+    sorted_colleges=College.sort_colleges("closing_rank")
+    return render(request,r"home\index2.html",{"data_list": enumerate(sorted_colleges)})
+    # return HttpResponse(KMIT.name)
+
+
+
