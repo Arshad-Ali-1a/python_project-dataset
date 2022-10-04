088ebf58f6b5801a5f6910d7aba131d1723d1f44
Arshad-Ali-1a <21bd1a661acsma@gmail.com>
2022-10-04 21:31:26 +0530

    removed viewport meta from tables (to make mobile friendly), and added distance,probability to sorted table, and added localStorage to remember form rank, gender,etc, and some more stuff

--- sort_colleges/static/sort_colleges/index.js
+++ sort_colleges/static/sort_colleges/index.js
@@ -16,4 +16,38 @@ function get_location(){
 
 document.addEventListener("DOMContentLoaded",(event) => {
     get_location();
+
+    //putting into form
+    if(rank=localStorage.getItem("rank")){
+
+        document.querySelector("#id_form_rank").value=rank;
+
+        gender=localStorage.getItem("gender");
+        document.querySelector(`input[value="${gender}"]`).checked=true;
+
+        category=localStorage.getItem("category");
+        document.querySelector(`input[value="${category}"]`).checked=true;
+    }
+
+    //storing in localstorage.
+    var frm= document.querySelector('form[id="details"]')
+    frm.onsubmit=() => {
+        // localStorage.getItem("rank")
+        const form = new FormData(frm);
+        const data = Object.fromEntries(form); // data is an js object.
+
+        rank=data["form_rank"]
+        gender=data["form_gender"]
+        category=data["form_category"]
+
+
+        localStorage.setItem("rank",rank)
+        localStorage.setItem("gender",gender)
+        localStorage.setItem("category",category)
+
+        console.log(rank,gender,category)
+
+    }
+
 });
+
